6d1f301a9a56b349ea95b9b3df91c2a584eb73a3
nav2924 <navi29012004@gmail.com>
2024-02-21 14:05:31 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { aboutus } from './pages/aboutus';
 
 function App() {
   return (
 <div>
+<aboutus />
     </div>
   );
 }
