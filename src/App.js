ca54d2d2088383cee6e3cf6473881c124a77ac77
Athul Nambiar <dev.athulnambiar@gmail.com>
2024-02-22 22:29:56 +0530

    contactus

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import * as React from 'react';
 import Aboutus from './pages/aboutus';
+import Contactus from './pages/contactus';
+import Members from './pages/Members';
 
 function App() {
   return (
     <div>
       <Aboutus />
+      <Contactus/>
+      <Members/>
     </div>
   );
 }
