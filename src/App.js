b51a9b3f0eaa3080a8a02ae7b7814ec4ea45f45c
nav2924 <navi29012004@gmail.com>
2024-02-23 14:03:58 +0530

    aboutus

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import * as React from 'react';
 import Aboutus from './pages/aboutus';
+import Contactus from './pages/contactus';
 
 function App() {
   return (
     <div>
       <Aboutus />
+      <Contactus />
     </div>
   );
 }
